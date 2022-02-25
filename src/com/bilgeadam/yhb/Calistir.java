d286a23f39720742dcb9f2ea13f9d28a925eb7c7
Canberk <canberkerdem@outlook.com>
2022-02-25 22:26:38 +0300

    Polymorphism

---
+++ src/com/bilgeadam/yhb/Calistir.java
@@ -0,0 +1,50 @@
+package com.bilgeadam.yhb;
+
+public class Calistir {
+
+//    public void seslendir(Kopek kopek)
+//    {
+//        kopek.sesCikar();
+//    }
+//    public void seslendir(Kedi kedi)
+//    {
+//        kedi.sesCikar();
+//    }
+
+    //Yukarıdaki gibi yapmak yerine asagıdaki gibi yap
+    //Her yeni hayvan eklendiğinde en bastan yeni hayvan yazma bunu kullan
+
+    public void seslendir(Hayvan hayvan)
+    {
+        hayvan.sesCikar();
+    }
+
+    public static void main(String[] args) {
+
+        Calistir calistir = new Calistir();
+
+        //Kopek kopek1 = new Kopek();
+        Hayvan kopek1 = new Kopek();
+        kopek1.setNo(1);
+        kopek1.setAd("Garip");
+        kopek1.setAgirlik(30);
+        kopek1.setUzunluk(1.5);
+        calistir.seslendir(kopek1);
+
+        //Kedi kedi1 = new Kedi();
+        Hayvan kedi1 = new Kedi();
+        kedi1.setNo(2);
+        kedi1.setAd("Sarman");
+        kedi1.setAgirlik(5);
+        kedi1.setUzunluk(0.4);
+
+        calistir.seslendir(kedi1);
+
+        Hayvan esek1 = new Esek();
+        esek1.setNo(3);
+        esek1.setAd("Karakacan");
+        esek1.setAgirlik(110);
+        esek1.setUzunluk(2.5);
+
+    }
+}
---
+++ src/com/bilgeadam/yhb/Esek.java
@@ -0,0 +1,9 @@
+package com.bilgeadam.yhb;
+
+public class Esek extends Hayvan {
+
+    public void  sesCikar()
+    {
+        System.out.println("Aaaaaaaaaa iiiiiiiiiii");
+    }
+}
